24eda6d4b686e5b5ec907dcfbcdbf049bd905210
신영환 <sin-yeonghwan@sin-yeonghwan-ui-macbook-air.local>
2018-09-24 21:51:44 +0900

    Initial Commit

---
+++ ex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ex
+//
+//  Created by 신영환 on 2018. 9. 24..
+//  Copyright © 2018년 신영환. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
